e2ad79465525f656b6b893df2abb1e22c251d8af
lucydu <746674534@qq.com>
2018-12-31 15:26:09 +0800

    Update x.py
    aif

--- x.py
+++ x.py
@@ -1,5 +1,4 @@
-#resource_key=item['resource_key']
-        # dict={'ar-SA': 17, 'bn-BD': 18, 'ca': 19, 'hr': 20, 'cs-CZ': 21, 'da-DK': 22, 'nl-NL': 15, 'tl-PH': 8, 'fi-FI': 23, 'fr-FR': 11, 'de-DE': 24, 'el-GR': 25, 'gu-IN': 26, 'he-IL': 27, 'hi-IN': 28, 'hu-HU': 29, 'it-IT': 12, 'ja-JP': 30, 'kn-IN': 31, 'ko-KR': 32, 'ms-MY': 5, 'ml-IN': 33, 'mr-IN': 34, 'nb-NO': 35, 'fa-IR': 36, 'pl-PL': 14, 'pt-PT': 37, 'pa-IN': 38, 'ro-RO': 39, 'ru-RU': 40, 'sk-SK': 41, 'sl-SI': 42, 'es-ES': 13, 'sv-SE': 43, 'ta-IN': 44, 'te-IN': 45, 'th-TH': 7, 'tr-TR': 16, 'uk-UA': 46, 'vi-VN': 47}
+       # dict={'ar-SA': 17, 'bn-BD': 18, 'ca': 19, 'hr': 20, 'cs-CZ': 21, 'da-DK': 22, 'nl-NL': 15, 'tl-PH': 8, 'fi-FI': 23, 'fr-FR': 11, 'de-DE': 24, 'el-GR': 25, 'gu-IN': 26, 'he-IL': 27, 'hi-IN': 28, 'hu-HU': 29, 'it-IT': 12, 'ja-JP': 30, 'kn-IN': 31, 'ko-KR': 32, 'ms-MY': 5, 'ml-IN': 33, 'mr-IN': 34, 'nb-NO': 35, 'fa-IR': 36, 'pl-PL': 14, 'pt-PT': 37, 'pa-IN': 38, 'ro-RO': 39, 'ru-RU': 40, 'sk-SK': 41, 'sl-SI': 42, 'es-ES': 13, 'sv-SE': 43, 'ta-IN': 44, 'te-IN': 45, 'th-TH': 7, 'tr-TR': 16, 'uk-UA': 46, 'vi-VN': 47}
         #dict = {'mr-IN': 34}
         # for key in dict.keys():
             # locale_id = dict[key]
